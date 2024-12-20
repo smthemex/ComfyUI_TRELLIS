a4945a3821ccf1f3b9f405bc38e72e4acca8b2ea
smthemex <138738845+smthemex@users.noreply.github.com>
2024-12-20 16:13:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import folder_paths
 
 MAX_SEED = np.iinfo(np.int32).max
 
-def image_to_3d(pipeline,image,preprocess_image:bool,covert2video:bool,trial_id: str, seed: int,ss_guidance_strength: float, ss_sampling_steps: int, slat_guidance_strength: float, slat_sampling_steps: int,mesh_simplify,texture_size,mode):
+def image_to_3d(pipeline,image,preprocess_image:bool,covert2video:bool,trial_id: str, seed: int,ss_guidance_strength: float, ss_sampling_steps: int, slat_guidance_strength: float, slat_sampling_steps: int,mesh_simplify,texture_size,mode,is_multiimage,Gaussians2PLY,multiimage_algo):
     """
     Convert an image to a 3D model.
 
@@ -24,26 +24,56 @@ def image_to_3d(pipeline,image,preprocess_image:bool,covert2video:bool,trial_id:
     """
     # if randomize_seed:
     #     seed = np.random.randint(0, MAX_SEED)
-    outputs = pipeline.run(
-        image,
-        seed=seed,
-        formats=["gaussian", "mesh"],
-        preprocess_image=preprocess_image,
-        sparse_structure_sampler_params={
-            "steps": ss_sampling_steps,
-            "cfg_strength": ss_guidance_strength,
-        },
-        slat_sampler_params={
-            "steps": slat_sampling_steps,
-            "cfg_strength": slat_guidance_strength,
-        },
-    )
+    if not  is_multiimage:
+        outputs = pipeline.run(
+            image,
+            seed=seed,
+            formats=["gaussian", "mesh"],
+            preprocess_image=preprocess_image,
+            sparse_structure_sampler_params={
+                "steps": ss_sampling_steps,
+                "cfg_strength": ss_guidance_strength,
+            },
+            slat_sampler_params={
+                "steps": slat_sampling_steps,
+                "cfg_strength": slat_guidance_strength,
+            },
+        )
+    else:
+        outputs = pipeline.run_multi_image(
+            image,
+            seed=seed,
+            formats=["gaussian", "mesh"],
+            preprocess_image=preprocess_image,
+            sparse_structure_sampler_params={
+                "steps": ss_sampling_steps,
+                "cfg_strength": ss_guidance_strength,
+            },
+            slat_sampler_params={
+                "steps": slat_sampling_steps,
+                "cfg_strength": slat_guidance_strength,
+            },
+            mode=multiimage_algo,
+        )
+      
     if covert2video:
-        video = render_utils.render_video(outputs['gaussian'][0], num_frames=120)['color']
-        video_geo = render_utils.render_video(outputs['mesh'][0], num_frames=120)['normal']
-        video = [np.concatenate([video[i], video_geo[i]], axis=1) for i in range(len(video))]
         video_path = f"{trial_id}.mp4"
-        imageio.mimsave(video_path, video, fps=15)
+        if is_multiimage:
+            video_gs = render_utils.render_video(outputs['gaussian'][0])['color']
+            video_mesh = render_utils.render_video(outputs['mesh'][0])['normal']
+            video = [np.concatenate([frame_gs, frame_mesh], axis=1) for frame_gs, frame_mesh in
+                     zip(video_gs, video_mesh)]
+            imageio.mimsave(video_path, video, fps=30)
+        else:
+            video = render_utils.render_video(outputs['gaussian'][0])['color']
+            imageio.mimsave(f"{trial_id}_gs.mp4", video, fps=30)
+            video = render_utils.render_video(outputs['radiance_field'][0])['color']
+            imageio.mimsave(f"{trial_id}_rf.mp4", video, fps=30)
+            video = render_utils.render_video(outputs['mesh'][0])['normal']
+            imageio.mimsave(f"{trial_id}_mesh.mp4", video, fps=30)
+       
+    if Gaussians2PLY:
+        outputs['gaussian'][0].save_ply(f"{trial_id}.ply")
     
     glb = postprocessing_utils.to_glb(
         outputs['gaussian'][0],
